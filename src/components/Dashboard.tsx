a7e10556b56a34e9162d1d4ba0a261f8f87a90c8
Xenia <xenia.klyukina@gmail.com>
2023-01-19 10:42:49 +0200

    create base layout, add some dependencies

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Dashboard: React.FC = () => {
+    return <>
+        <div className="container__login">
+            <p>Dashboard</p>
+        </div>
+    </>;
+}
