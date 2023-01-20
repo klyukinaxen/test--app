6eed37dd797db97f69932b27f3fd1abd40a0fea0
Xenia <xenia.klyukina@gmail.com>
2023-01-20 09:19:36 +0200

    add npm classnames, add todoItem

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,6 +1,35 @@
 import React from "react";
+import { ITodo } from "../interfaces";
+import classNames from 'classnames'
+import Checkbox from "@mui/material/Checkbox"
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useAppDispatch } from "../hook";
+import { toggleComplete, removeTodo } from "../store/todoSlice";
 
-export const TodoItem = () => {
+interface ITodoListProps {
+    todo: ITodo,
+}
+
+export const TodoItem: React.FC<ITodoListProps> = ({ todo }) => {
+    const dispatch = useAppDispatch();
+
+    const removeHandler = (event: React.MouseEvent, id: number) => {
+        event.preventDefault()
+        dispatch(removeTodo(id))
+    }
+    const classes = classNames('todo', { completed: todo.completed })
     return (<>
+        <li className={classes} key={todo.id}>
+            <label>
+                <div className="todo-item">
+                    <Checkbox
+                        checked={todo.completed}
+                        onClick={() => dispatch(toggleComplete(todo.id))}
+                    />
+                    <span>{todo.title}</span>
+                    <DeleteIcon className="delete-icon" onClick={event => removeHandler(event, todo.id)} />
+                </div>
+            </label>
+        </li>
     </>)
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,24 +1,14 @@
 import React from "react";
 import { ITodo } from "../interfaces";
-import Checkbox from "@mui/material/Checkbox"
-import DeleteIcon from '@mui/icons-material/Delete';
-import { useAppDispatch } from "../hook";
-import { toggleComplete, removeTodo } from "../store/todoSlice";
+import { TodoItem } from "./TodoItem";
 
 interface ITodoListProps {
     todos: ITodo[]
 }
 
 export const TodoList: React.FC<ITodoListProps> = ({ todos }) => {
-    const dispatch = useAppDispatch();
-
     if (todos.length === 0) {
         return (<p>Список дел пуст</p>)
-    }
-
-    const removeHandler = (event: React.MouseEvent, id: number) => {
-        event.preventDefault()
-        dispatch(removeTodo(id))
     }
 
     return <>
@@ -26,26 +16,9 @@ export const TodoList: React.FC<ITodoListProps> = ({ todos }) => {
             <h3>Список дел: </h3>
             <div className="container__todos_list">
                 <div>
-                    {/* TODO: вынести в TodoItem */}
-                    {todos.map(todo => {
-                        const classes = ['todo']
-                        if (todo.completed) { classes.push('completed') }
-                        return (
-                            // TODO: переделать join на https://www.npmjs.com/package/classnames
-                            <li className={classes.join(' ')} key={todo.id}>
-                                <label>
-                                    <div className="todo-item">
-                                        <Checkbox
-                                            checked={todo.completed}
-                                            onClick={() => dispatch(toggleComplete(todo.id))}
-                                        />
-                                        <span>{todo.title}</span>
-                                        <DeleteIcon className="delete-icon" onClick={event => removeHandler(event, todo.id)} />
-                                    </div>
-                                </label>
-                            </li>
-                        )
-                    })}
+                    {todos.map(todo =>
+                        <TodoItem todo={todo}></TodoItem>
+                    )}
                 </div>
             </div>
         </div>
